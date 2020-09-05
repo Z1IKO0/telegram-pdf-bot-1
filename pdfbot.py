a2f2d884a9292fe241394eca2f463e05a5e40ab6
TCA <52579361+ajithtca@users.noreply.github.com>
2020-09-05 09:14:26 +0530

    Update pdfbot.py

--- pdfbot.py
+++ pdfbot.py
@@ -16,7 +16,6 @@ import logging
 import uuid
 import shutil
 
-from config import Config
 import piexif
 import img2pdf
 from img2pdf import NegativeDimensionError, UnsupportedColorspaceError, ImageOpenError, \
@@ -204,8 +203,8 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    TOKEN = os.getenv("TELEGRAM_TOKEN")
-    updater = Updater(TOKEN, use_context=True)
+    
+    updater = Updater(1315723808:AAHzEfYiBWs6Qlh327IHBp5aejIWie_zMj0, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
